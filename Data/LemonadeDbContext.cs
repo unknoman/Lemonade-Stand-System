ed09a7b579a8fb8f42e42a401014c2a08d32679c
emanuel oviedo <emanueloviedo9@gmail.com>
2024-03-11 03:30:54 -0300

    dbContext update

--- Data/LemonadeDbContext.cs
+++ Data/LemonadeDbContext.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace Data
 {
     public class LemonadeDbContext : DbContext
     {
-    }
+
+        public DbSet<ClientOrder> ClientOrders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        }
 }
