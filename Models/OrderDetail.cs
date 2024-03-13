f8cbe64037c846e57301081bf06ea3deb859d6b8
emanuel oviedo <emanueloviedo9@gmail.com>
2024-03-12 21:53:29 -0300

    Models update

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -6,10 +6,12 @@ namespace Models
         public int id { get; set; }
         public int order { get; set; }
         public int product { get; set; }
+
+        public int supplies { get; set; }
         public float unityPrice { get; set; }
         public float quantity { get; set; }
 
-
+        public SuppliesOrder suppliesOrder { get; set; } = new SuppliesOrder();
         public ClientOrder clientOrder { get; set; } = new ClientOrder();
 
     }
